7023184be7aa6c0f8c9aaae0a8a6f3f546657265
GeunHye <wheeinjj417@gmail.com>
2025-01-16 03:43:30 +0900

    -Chapter8: 스프링 시큐리티로 로그인/로그아웃, 회원가입 구현하기

---
+++ src/main/java/me/kimgeunhye/springbootdeveloper/dto/AddUserRequest.java
@@ -0,0 +1,14 @@
+package me.kimgeunhye.springbootdeveloper.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddUserRequest {
+    private String email;
+    private String password;
+}
+
+
