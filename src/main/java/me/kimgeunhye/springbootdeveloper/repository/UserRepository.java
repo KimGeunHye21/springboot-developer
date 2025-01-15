b0a299287de50d96286bffc2744ea0e7be896eb9
GeunHye <wheeinjj417@gmail.com>
2025-01-16 03:43:30 +0900

    -Chapter8: 스프링 시큐리티로 로그인/로그아웃, 회원가입 구현하기

---
+++ src/main/java/me/kimgeunhye/springbootdeveloper/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package me.kimgeunhye.springbootdeveloper.repository;
+
+import me.kimgeunhye.springbootdeveloper.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+}
